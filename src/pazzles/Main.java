9799c437928c333d22119c0f768ccb7d5a9c340e
Andrey Pomazkin <apomazkin@skbkontur.ru>
2017-02-20 15:39:47 +0500

    isOdd

---
+++ src/pazzles/Main.java
@@ -0,0 +1,13 @@
+package pazzles;
+
+import static pazzles.Odd.isOdd;
+
+/**
+ * Created on 20.02.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("isOdd(1): " + isOdd(1));
+        System.out.println("isOdd(2): " + isOdd(2));
+    }
+}
