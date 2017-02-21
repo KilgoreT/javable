4d87033adeb20c3a38b8953627b56275bfdf0b05
Andrey Pomazkin <apomazkin@skbkontur.ru>
2017-02-21 13:50:30 +0500

    LongDivision

--- src/pazzles/LongDivision.java
+++ src/pazzles/LongDivision.java
@@ -20,6 +20,7 @@ public class LongDivision {
          * MICROS_PER_DAY_L
          * первый множитель в формате long,
          * поэтому дальнейшее вычисление будет происходить с приведением к long
+         * ВНИМАНИЕ: лучше использовать ЗАГЛАВНУЮ L, что бы не путать с единицей(1 и l).
          */
         final long MICROS_PER_DAY_L = 24L * 60 * 60 * 1000 * 1000;
         final long MILLIS_PER_DAY_L = 24L * 60 * 60 * 1000;
