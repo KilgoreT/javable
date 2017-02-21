c814b691d814da828b97ef59f5333459dd459757
Andrey Pomazkin <apomazkin@skbkontur.ru>
2017-02-21 17:02:51 +0500

    Hex

---
+++ src/pazzles/HexMixedComputation.java
@@ -0,0 +1,37 @@
+package pazzles;
+
+/**
+ * Created by apomazkin on 21.02.2017.
+ */
+public class HexMixedComputation {
+    public static void hexAddition() {
+        System.out.println(
+                Long.toHexString(0x100000000L + 0xcafebabe)
+                /**
+                 * Expected result: 1cafebabe
+                 * Но получаем: cafebabe.
+                 * Почему?
+                 * Если отрицательные десятичные числа видно по знаку (-)
+                 * то hex пишутся без знака.
+                 * Как понять, что hex отрицательно?
+                 * Если первый бит(самый старший) установен в 1.
+                 * 0xcafebabe == ‭11001010111111101011101010111110‬ b
+                 * значит 0xcafebabe - отрицательно.
+                 * Приводим 0xcafebabe к Long, получаем:
+                 *      0xffffffffcafebabeL
+                 *    + 0x0000000100000000L
+                 *    = --------------------
+                 *      0x00000000cafebabeL
+                 *
+                 * Исправляется указанием типа Long для второго слагаемого.
+                 */
+        );
+        System.out.println(
+                Long.toHexString(0x100000000L + 0xcafebabeL)
+                /**
+                 * Исправляется указанием типа Long для второго слагаемого.
+                 * Вывод: не использовать в выражениях аргументы разных типов.
+                 */
+        );
+    }
+}
--- src/pazzles/Main.java
+++ src/pazzles/Main.java
@@ -1,6 +1,6 @@
 package pazzles;
 
-import static pazzles.LongDivision.longDivision;
+import static pazzles.HexMixedComputation.hexAddition;
 
 /**
  * Created on 20.02.2017.
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println("isOdd(1): " + isOdd(1));
         //System.out.println("isOdd(2): " + isOdd(2));
-        longDivision();
+       // longDivision();
+         hexAddition();
     }
 }
