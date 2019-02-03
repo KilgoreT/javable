610e46b9e42ec23c64d723c470c0374b5a7764c4
ki16or <ki16or@gmail.com>
2019-02-04 01:08:59 +0500

    binary tree

--- src/dataStructure/tree/_00_binary/Tree.java
+++ src/dataStructure/tree/_00_binary/Tree.java
@@ -147,6 +147,131 @@ public class Tree {
     }
 
 
-    void delete(int key) {}
+    boolean delete(int key) {
 
+        Node current = root;
+        Node parent = root;
+        boolean isLeftChild = true;
+
+        // поиск удаляемого узла
+        while (current.getKey() != key) {
+            if (key < current.getKey()) {
+                isLeftChild = true;
+                parent = current;
+                current = parent.getLeft();
+            } else {
+                isLeftChild = false;
+                parent = current;
+                current = parent.getRight();
+            }
+            if (current == null) {
+                return false;
+            }
+        }
+
+        // если у удаляемого нет потомков
+        if (hasNoChild(current)) {
+            if (current == root) {
+                root = null;
+                return true;
+            }
+            if (isLeftChild) {
+                parent.setLeft(null);
+                return true;
+            }
+            if (!isLeftChild) {
+                parent.setRight(null);
+                return true;
+            }
+        }
+
+        // если у удаляемого левый потомок
+        if (current.getRight() == null) {
+            if (current == root) {
+                root = current.getLeft();
+                return true;
+            }
+            if (isLeftChild) {
+                parent.setLeft(current.getLeft());
+                return true;
+            }
+            if (!isLeftChild) {
+                parent.setRight(current.getLeft());
+                return true;
+            }
+        }
+
+        // если у удаляемого правый потомок
+        if (current.getLeft() == null) {
+            if (current == root) {
+                root = current.getRight();
+                return true;
+            }
+            if (isLeftChild) {
+                parent.setLeft(current.getRight());
+                return true;
+            }
+            if (!isLeftChild) {
+                parent.setRight(current.getRight());
+                return true;
+            }
+        }
+
+        // если у удаляемого есть оба потомка
+        Node successor = getSuccessor(current);
+        if (current == root) {
+            root = successor;
+            return true;
+        }
+        if (isLeftChild) {
+            parent.setLeft(successor);
+            successor.setLeft(current.getLeft());
+            return true;
+        }
+        if (!isLeftChild) {
+            parent.setRight(successor);
+            successor.setLeft(current.getLeft());
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Чтобы удалить ноду с двумя потомками,
+     * нужно заменить ее на преемника(successor).
+     * Преемник - та нода правого потомка,
+     * которая имеет наименьший ключь(но этот ключ больше чем у удаляемой ноды)
+     * Т.е.
+     * 1. преемник вырезается из правого потомка удаляемой ноды и подставляется на место удаляемой ноды
+     * 2. правым потомком преемника становится правый потомок удаляемой ноды.
+     * @param delNode удаляемая нода
+     * @return нода-преемник
+     */
+    private Node getSuccessor(Node delNode) {
+
+        Node successorParent = delNode;
+        Node successor = delNode;
+        Node current = delNode.getRight();
+
+        while (current != null) {
+            successorParent = successor;
+            successor = current;
+            current = current.getLeft();
+        }
+        // проверяется тот случай, когда правый потомок является преемником,
+        // т.е. правый потомок не имеет левой ветки.
+        if (successor != delNode.getRight()) {
+            // у преемника по определению нет левых потомков, но могут быть правые
+            // поэтому предку преемника добавляем в левую ветвь правого потомка преемника
+            successorParent.setLeft(successor.getRight());
+            // преемнику в правую ветвь добавляем правую ветвь удаляемой ноды.
+            successor.setRight(delNode.getRight());
+        }
+        return successor;
+    }
+
+
+    private boolean hasNoChild(Node node) {
+        return node.getLeft() == null && node.getRight() == null;
+    }
 }
--- src/dataStructure/tree/_00_binary/_TreeApp.java
+++ src/dataStructure/tree/_00_binary/_TreeApp.java
@@ -7,7 +7,7 @@ public class _TreeApp {
         tree.insert( 25, "*");
         tree.insert( 75, "-");
         tree.insert( 15, "15");
-        tree.insert( 12, "12");
+        tree.insert( 35, "35");
         tree.insert( 70, "70");
         tree.insert( 99, "99");
 
@@ -32,5 +32,29 @@ public class _TreeApp {
 
         System.out.println("\nПоиск максимума:");
         System.out.println(tree.getMaximum().showNode());
+
+        System.out.println("\nУдаление: 99");
+        if (tree.delete(99)) {
+            System.out.println("99 deleted!");
+        } else {
+            System.out.println("99 not found!");
+        }
+        tree.inOrder();
+
+        System.out.println("\nУдаление: 75");
+        if (tree.delete(75)) {
+            System.out.println("75 deleted!");
+        } else {
+            System.out.println("75 not found!");
+        }
+        tree.inOrder();
+
+        System.out.println("\nУдаление: 25");
+        if (tree.delete(25)) {
+            System.out.println("25 deleted!");
+        } else {
+            System.out.println("25 not found!");
+        }
+        tree.inOrder();
     }
 }
