01df90c6046bd4cc2257e2d32c2b42911c306ef3
ki16or <ki16or@gmail.com>
2019-04-11 00:16:37 +0500

    Графы

--- src/dataStructure/_06_graph/Graph.java
+++ src/dataStructure/_06_graph/Graph.java
@@ -8,6 +8,7 @@ public class Graph {
     private int nVerts;
     private StackX theStack;
     private Queue theQueue;
+    private char sortedArray[];
 
     public enum V {
         A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P
@@ -24,6 +25,7 @@ public class Graph {
         }
         theStack = new StackX();
         theQueue = new Queue();
+        sortedArray = new char[MAX_VERTS];
     }
 
     public void addVertex(char label) {
@@ -33,6 +35,10 @@ public class Graph {
     public void addEdge(V start, V end) {
         adjMat[start.ordinal()][end.ordinal()] = 1;
         adjMat[end.ordinal()][start.ordinal()] = 1;
+    }
+
+    public void addDirectedEdge(V start, V end) {
+        adjMat[start.ordinal()][end.ordinal()] = 1;
     }
 
     public void displayVertex(int v) {
@@ -97,6 +103,113 @@ public class Graph {
         }
         for (int i = 0; i < nVerts; i++) {
             vertexList[i].wasVisited = false;
+        }
+    }
+
+    /**
+     * Построение минимального остовного дерева.
+     * (Minimum Spanning Tree): где,
+     * количество ребер всегда на одно меньше количества вершин.
+     */
+    public void mst() {
+
+        vertexList[0].wasVisited = true;
+        theStack.push(0);
+
+        while (!theStack.isEmpty()) {
+            int currentVertex = theStack.peek();
+            int v = getAdjUnvisitedVertex(currentVertex);
+            if (v == -1) {
+                theStack.pop();
+            } else {
+                vertexList[v].wasVisited = true;
+                theStack.push(v);
+
+                displayVertex(currentVertex);
+                displayVertex(v);
+                System.out.print(" ");
+            }
+        }
+        for (int i = 0; i < nVerts; i++) {
+            vertexList[i].wasVisited = false;
+        }
+    }
+
+    /**
+     * Топологическая сортировка,
+     *  например, когда нужно отобразить зависимые элементы от других.
+     * На выходе получаем топологически отсортированный граф.
+     * Моделирование планирования заданий(элементов) с использованием графов,
+     *  называется "анализом критических путей".
+     *
+     *  Топологическая сортировка должна применяться к графам, не содержащим циклов,
+     *  т.е. направленным ациклическим графам.
+     */
+    public void topo() {
+        // сохранение количества вершин
+        int orig_nVerts = nVerts;
+
+        while (nVerts > 0) {
+            int currentVertex = noSuccessors();
+            if (currentVertex == -1) {
+                System.out.println("Error: Graph has cycles!");
+                return;
+            }
+            sortedArray[nVerts - 1] = vertexList[currentVertex].label;
+            deleteVertex(currentVertex);
+        }
+        System.out.print("Topology sorted order: ");
+        for (int index = 0; index < orig_nVerts; index++) {
+            System.out.print(sortedArray[index]);
+        }
+        System.out.println("");
+    }
+
+    /**
+     * Метод возвращает вершину, не имеющую преемников
+     * @return - вершина
+     */
+    private int noSuccessors() {
+        boolean hasEdge;
+        for (int row = 0; row < nVerts; row++) {
+            hasEdge = false;
+            for (int col = 0; col < nVerts; col++) {
+                if (adjMat[row][col] > 0) {
+                    hasEdge = true;
+                    break;
+                }
+            }
+            if (!hasEdge) {
+                return row;
+            }
+        }
+        return -1;
+    }
+
+    private void deleteVertex(int deletedVertex) {
+        if (deletedVertex != nVerts - 1) {
+            for (int index = deletedVertex; index < nVerts - 1; index++) {
+                vertexList[index] = vertexList[index + 1];
+            }
+            for (int row = deletedVertex; row < nVerts - 1; row++) {
+                moveRowUp(row, nVerts);
+            }
+            for (int col = deletedVertex; col < nVerts - 1; col++) {
+                moveColLeft(col, nVerts - 1);
+            }
+            nVerts--;
+        }
+    }
+
+    private void moveRowUp(int row, int length) {
+        for (int col = 0; col < length; col++) {
+            adjMat[row][col] = adjMat[row + 1][col];
+        }
+    }
+
+    private void moveColLeft(int col, int length) {
+        for (int row = 0; row < length; row++) {
+            adjMat[row][col] = adjMat[row][col + 1];
         }
     }
 }
